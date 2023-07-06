6ae6367f539c535e39e518b18f17576610461478
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-07-06 13:59:00 -0400

    update

--- app/src/App.js
+++ app/src/App.js
@@ -1,38 +1,66 @@
-import {useState} from "react";
+import { useState } from "react";
 import React from "react";
 import './App.css';
 
+let toDoListItem = {
+  "className": "text-decoration-line-through",
+  "toDo": "What needs to be done?"
+
+}
 
 
 
+function App(props) {
+  const [data, setData] = useState([]);
+  const [tempData, setTemp] = useState("");
 
-function App() {
-  const [initialText, setInitialText] = useState("");
-  let toDoListItem = {
-    "className": "text-decoration-line-through",
-    "initialText": ""
-  
+  function handleEvent(e) {
+    if (e.key === 'Enter') {
+
+      //  get from tempData 
+      // create new item stored using setData
+      let newToDo = setData(tempData);
+      // get my to do list items
+      let tempList = data; // proxy array
+
+      // add new obj to tempList
+      let newTempItem = { toDo: tempData };
+
+      tempList.push(newTempItem);
+
+      // set state by pushing proxy array
+      //setData(tempList);
+    }
   }
+
+  let filteredList = data.filter(item => data.toDo);
+
   return (
     <>
-    <div className="text-center">
-  <label className="text-center">
-  <input type="text" value={toDoListItem.initialText} 
-  onInputCapture={e => setInitialText(e.target.value)}
-  />
-    </label>
-    </div>
+      <form>
+        <div className="text-center">
+          <label className="text-center">
+            <input type="text" placeholder={tempData}
+              onChange={e => setTemp(e.target.value)}
+              onKeyDown={handleEvent}
+            />
+          </label>
+        </div>
+      </form>
 
-    <div className="App">
-      <header>
-    
-       
-        <p>
-          
-        </p>
-       
-      </header>
-    </div>
+      <div className="list">
+        <header>
+
+          {/*map through the data in state, display each toDoListItem*/}
+
+          {/*filteredLIst.map(item => item.toDoListItem.toDo)*/}
+          <ul className="text-center">
+            <li>
+              {toDoListItem.toDo}
+            </li>
+          </ul>
+        </header>
+      </div>
     </>
   );
 }
